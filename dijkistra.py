b11096b961309a39d305ae0b4bb45d360d51f386
Igor <igor.gutoo63@gmail.com>
2021-11-15 16:23:14 -0300

    Iniciando implementação

---
+++ dijkistra.py
@@ -0,0 +1,52 @@
+import math
+
+
+def dijkstra(grafo, s, destino):
+    dist = []
+    pred = []
+    Q = []
+    i = 0
+    excluidos = []
+    aux = grafo
+
+    for v in grafo:
+        dist.append(math.inf)
+        pred.append(None)
+        Q.append(i)
+        i += 1
+
+    dist[s] = 0
+    print("Algoritmo de Dijkstra")
+    print("Origem: ", s)
+    print("Destino: ", destino)
+    print("Processando...")
+
+    for q in Q:
+        menor, u = buscaMenor(dist, excluidos)
+        excluidos.append(u)
+        i = 0
+
+        for v in aux[u]:
+            if dist[v[0]] > dist[u] + v[1]:
+                dist[v[0]] = dist[u] + v[1]
+                pred[v[0]] = u
+            i += 1
+
+    return dist, pred
+
+
+def buscaMenor(lista, excluidos):
+    menor = math.inf
+    pos = 0
+    i = 0
+
+    while i < len(lista):
+        if i in excluidos:
+            i += 1
+            continue
+        if lista[i] < menor:
+            menor = lista[i]
+            pos = i
+        i += 1
+
+    return menor, pos
---
+++ main.py
@@ -0,0 +1,26 @@
+import dijkistra
+
+
+def main():
+    try:
+        file = input('Informe o arquivo: ')
+        # ./Datasets/toy.txt
+        origem = int(input('Origem: '))
+        destino = int(input('Destino: '))
+        with open(file, "r") as f:
+            header = f.readline().split()
+            G = [[] for _ in range(int(header[0]))]  # Tamanho do grafo
+
+            for line in f:
+                if len(line.split()) == 2:  # Pula a primeira linha contendo cabeçalho
+                    continue
+                valores = (*map(int, line.split()), )
+                G[valores[0]].append(valores[1:])
+
+            print(G)
+            print(dijkistra.dijkstra(G, origem, destino))
+    except:
+        print('Não foi posível ler o arquivo, tente novamente.')
+
+
+main()
